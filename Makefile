900980f62819df7b669f20e6ec96a1d0b87f0bb6
thxCode <thxcode0824@gmail.com>
2023-12-13 13:56:51 +0800

    refactor: generate schema
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- Makefile
+++ Makefile
@@ -18,14 +18,16 @@ help:
 	# Usage:
 	#
 	#   * [dev] `make generate`, generate README file.
-	#           - `make generate examples/hello-world` only generate README file under examples/hello-world directory.
+	#           - `make generate examples/hello-world` only generate docs and schema under examples/hello-world directory.
+	#           - `make generate docs examples/hello-world` only generate README file under examples/hello-world directory.
+	#           - `make generate schema examples/hello-world` only generate schema.yaml under examples/hello-world directory.
 	#
 	#   * [dev] `make lint`, check style and security.
 	#           - `LINT_DIRTY=true make lint` verify whether the code tree is dirty.
 	#           - `make lint examples/hello-world` only verify the code under examples/hello-world directory.
 	#
 	#   * [dev] `make test`, execute unit testing.
-	#           - `make test example/hello-world` only test the code under examples/hello-world directory.
+	#           - `make test complete/hello-world` only test the code under examples/hello-world directory.
 	#
 	#   * [ci]  `make ci`, execute `make generate`, `make lint` and `make test`.
 	#
@@ -33,4 +35,4 @@ help:
 
 
 .DEFAULT_GOAL := ci
-.PHONY: $(targets) examples $(examples) modules $(modules) tests
+.PHONY: $(targets) examples $(examples) modules $(modules) tests docs schema
--- hack/generate.sh
+++ hack/generate.sh
@@ -8,6 +8,54 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 source "${ROOT_DIR}/hack/lib/init.sh"
 
 function generate() {
+  local mode="${1:-all}"
+
+  case $mode in
+    docs)
+      shift 1
+      generate_doc "${ROOT_DIR}" "$@"
+      ;;
+    schema)
+      shift 1
+      generate_schema "${ROOT_DIR}" "$@"
+      ;;
+    *)
+      generate_doc "${ROOT_DIR}" "$@"
+      generate_schema "${ROOT_DIR}" "$@"
+      ;;
+  esac
+}
+
+function generate_doc() {
+  local target="$1"
+  shift 1
+
+  if [[ $# > 0 ]]; then
+    for subdir in "$@"; do
+      local path="${target}/${subdir}"
+      local tfs
+      tfs=$(seal::util::find_files "${path}" "*.tf")
+
+      if [[ -n "${tfs}" ]]; then
+        seal::terraform::docs "${path}" --config="${target}/.terraform-docs.yml"
+      else
+        seal::log::warn "There is no Terraform files under ${path}"
+      fi
+    done
+
+    return 0
+  fi
+
+  seal::terraform::docs "${target}" --config="${target}/.terraform-docs.yml" --recursive
+  local examples=()
+  # shellcheck disable=SC2086
+  IFS=" " read -r -a examples <<<"$(seal::util::find_subdirs ${target}/examples)"
+  for example in "${examples[@]}"; do
+    seal::terraform::docs "${target}/examples/${example}" --config="${target}/.terraform-docs.yml"
+  done
+}
+
+function generate_schema() {
   local target="$1"
   shift 1
 
@@ -27,17 +75,13 @@ function generate() {
     return 0
   fi
 
-  seal::terraform::docs "${target}" --config="${target}/.terraform-docs.yml" --recursive
-  local examples=()
+  seal::walrus_cli::schema "${target}"
+  local sub_modules=()
   # shellcheck disable=SC2086
-  IFS=" " read -r -a examples <<<"$(seal::util::find_subdirs ${target}/examples)"
-  for example in "${examples[@]}"; do
-    seal::terraform::docs "${target}/examples/${example}" --config="${target}/.terraform-docs.yml"
+  IFS=" " read -r -a sub_modules <<<"$(seal::util::find_subdirs ${target}/modules)"
+  for module in "${sub_modules[@]}"; do
+    seal::walrus_cli::schema "${target}/modules/${module}"
   done
-
-  # fetch chart from https://hub.docker.com/r/bitnamicharts/rabbitmq/tags.
-  seal::helm::pull oci://registry-1.docker.io/bitnamicharts/rabbitmq:12.4.1 "${ROOT_DIR}/charts"
-
 }
 
 #
@@ -46,6 +90,9 @@ function generate() {
 
 seal::log::info "+++ GENERATE +++"
 
-generate "${ROOT_DIR}" "$@"
+generate "$@"
+
+# fetch chart from https://hub.docker.com/r/bitnamicharts/rabbitmq/tags.
+seal::helm::pull oci://registry-1.docker.io/bitnamicharts/rabbitmq:12.4.1 "${ROOT_DIR}/charts"
 
 seal::log::info "--- GENERATE ---"
